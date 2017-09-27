0432b3d5469728c5cce5dd3c0d762ffbd476250b
Aleksander Chromik <achromik@gmail.com>
2017-09-27 22:10:07 +0200

    FINISH - serving files

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+    file = require('fs');
+
+    server = http.createServer();
+server.on('request', function(request, response){
+    
+    if (request.method === 'GET' && request.url === '/') {
+        //response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        file.readFile('./index.html', 'utf-8', function(err, data){ 
+            response.write(data);
+            response.end();
+        });
+    } else {
+        file.readFile('./404.jpg', function(err, data){ 
+            response.write(data);
+            response.end();
+        });
+    }
+});
+
+server.listen(8080);
+
