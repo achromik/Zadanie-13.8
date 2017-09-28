29683c5094b943e0e419179475f1ec5a6d2b0178
Aleksander Chromik <achromik@gmail.com>
2017-09-28 10:12:24 +0200

    CHANGE - 404 status code

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ var http = require('http'),
     server = http.createServer();
 server.on('request', function(request, response){
     
-    if (request.method === 'GET' && request.url === '/') {
-        //response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if (request.method === 'GET' && (request.url === '/') ) {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
         file.readFile('./index.html', 'utf-8', function(err, data){ 
             response.write(data);
             response.end();
         });
     } else {
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'image/jpg');
+        
+        
+        //response.writeHead(404, {'Content-Type': 'image/jpg'});
         file.readFile('./404.jpg', function(err, data){ 
+            
             response.write(data);
             response.end();
         });
